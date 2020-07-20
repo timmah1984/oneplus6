5bc2208f0d5ca0d800b7e44b8d9f2d8336086fd4
AnierinB <anierinb@evolution-x.org>
2020-07-20 01:35:00 -0700

    enchilada: Move recovery deps to a more ideal place
    - Having these in device just doesn't make a lot of sense to me.
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,7 +23,7 @@ DEVICE_PATH := device/oneplus/enchilada
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # Prebuilt TWRP BLACK recovery
-TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := device/twrp/black/enchilada/ramdisk-recovery.cpio
+TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := recovery/twrp/black/device/oneplus/enchilada/ramdisk-recovery.cpio
 
 # inherit from the proprietary version
 -include vendor/oneplus/enchilada/BoardConfigVendor.mk
