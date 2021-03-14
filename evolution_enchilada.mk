96919a88ad3421ed0051c4b9ec1d463f649c6fc2
chandu078 <chandudyavanapelli03@gmail.com>
2021-03-14 17:08:13 +0000

    enchilada: Drop useless flags
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- evolution_enchilada.mk
+++ evolution_enchilada.mk
@@ -32,8 +32,6 @@ TARGET_BOOT_ANIMATION_RES := 1080
 
 # Gapps
 TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_WIFI_EXT := true
-WITH_GAPPS := true
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := evolution_enchilada
