c39c77b9fa7898c4767e77f728d7292b4cb20180
chandu078 <chandudyavanapelli03@gmail.com>
2021-03-14 12:43:27 +0000

    enchilada: Drop build fp/desc * No need anymore. Evo X overriding to latest redfin fingerprint * https://github.com/Evolution-X/frameworks_base/commit/bef8c6d1c53277e704b2edd1622d8b7703d78b0a
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- evolution_enchilada.mk
+++ evolution_enchilada.mk
@@ -46,7 +46,4 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlus6 \
-    PRODUCT_NAME=OnePlus6 \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210305.006 7119741 release-keys"
-
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys
+    PRODUCT_NAME=OnePlus6
